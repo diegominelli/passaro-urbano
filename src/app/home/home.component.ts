d8009e7a24d9abd382ea360c68e3832f7c37a735
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-13 20:07:38 -0300

    usando promises

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,6 +14,12 @@ export class HomeComponent implements OnInit {
   constructor(private ofertasService: OfertasService) {}
 
   ngOnInit() {
-    this.ofertas = this.ofertasService.getOfertas();
+    // this.ofertas = this.ofertasService.getOfertas();
+    this.ofertasService.getOfertas2().then(
+      (ofertas: Oferta[]) => {
+        this.ofertas = ofertas;
+      },
+      (param: any) => console.log(param)
+    );
   }
 }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -55,4 +55,15 @@ export class OfertasService {
   public getOfertas(): Oferta[] {
     return this.ofertas;
   }
+
+  public getOfertas2(): Promise<Oferta[]> {
+    return new Promise((resolve, reject) => {
+      let deu_certo: false;
+      if (deu_certo) {
+        resolve(this.ofertas);
+      } else {
+        reject({ codigo_erro: 404, mensagem_erro: 'Servidor não encontrado' });
+      }
+    });
+  }
 }
