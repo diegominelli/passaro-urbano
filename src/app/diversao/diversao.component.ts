3b1a93524012ebb2f5bf481d674e07b334b0ed90
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-16 13:34:47 -0300

    recuperando as oferas do tipo diversao

--- src/app/diversao/diversao.component.ts
+++ src/app/diversao/diversao.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Oferta } from '../shared/oferta.model';
+import { OfertasService } from './../ofertas.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.scss']
+  styleUrls: ['./diversao.component.scss'],
+  providers: [OfertasService],
 })
-export class DiversaoComponent {
+export class DiversaoComponent implements OnInit {
+  public ofertas: Oferta[];
 
+  constructor(private ofertasService: OfertasService) {}
+
+  ngOnInit(): void {
+    this.ofertasService
+      .getOfertasPorCategoria('diversao')
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas;
+      });
+  }
 }
