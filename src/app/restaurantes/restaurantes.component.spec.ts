6fdf77583509e73729f86f5fad8d8175332776a1
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-16 10:36:38 -0300

    criacao e desenvolvimento do componente restaurantes

---
+++ src/app/restaurantes/restaurantes.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RestaurantesComponent } from './restaurantes.component';
+
+describe('RestaurantesComponent', () => {
+  let component: RestaurantesComponent;
+  let fixture: ComponentFixture<RestaurantesComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RestaurantesComponent]
+    });
+    fixture = TestBed.createComponent(RestaurantesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/restaurantes/restaurantes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurantes',
+  templateUrl: './restaurantes.component.html',
+  styleUrls: ['./restaurantes.component.scss']
+})
+export class RestaurantesComponent {
+
+}
