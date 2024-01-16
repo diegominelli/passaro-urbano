7d95059284a505c838c518b11bc2f1d66c4f24ba
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-16 17:07:59 -0300

    criando as tabs Como usa e Onde fica

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,27 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Oferta } from './shared/oferta.model';
 import { Injectable } from '@angular/core';
 
+import { URL_API } from './app.api';
+
 @Injectable()
 export class OfertasService {
   constructor(private http: HttpClient) {}
 
   public getOfertas(): Promise<Oferta[]> {
     return this.http
-      .get(`http://localhost:3000/ofertas`)
+      .get(`${URL_API}`)
       .toPromise()
       .then((resposta: any) => resposta);
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
     return this.http
-      .get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+      .get(`${URL_API}?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta);
   }
 
   public getOfertaPorId(id: number): Promise<Oferta> {
     return this.http
-      .get(`http://localhost:3000/ofertas?id=${id}`)
+      .get(`${URL_API}?id=${id}`)
       .toPromise()
       .then((resposta: any) => {
         return resposta[0];
