758a5088def5f44e852bc146e7cbb0663f8db647
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-16 18:03:33 -0300

    recuperando dados de onde fica via http request

--- src/app/oferta/onde-fica/onde-fica.component.ts
+++ src/app/oferta/onde-fica/onde-fica.component.ts
@@ -1,3 +1,4 @@
+import { OfertasService } from './../../ofertas.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -5,10 +6,20 @@ import { ActivatedRoute } from '@angular/router';
   selector: 'app-onde-fica',
   templateUrl: './onde-fica.component.html',
   styleUrls: ['./onde-fica.component.scss'],
+  providers: [OfertasService],
 })
 export class OndeFicaComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  public ondeFica: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
+  ) {}
   ngOnInit(): void {
-    console.log(this.route.parent.snapshot.params['id']);
+    this.ofertasService
+      .getOndeFicaOfertaPorId(this.route.parent.snapshot.params['id'])
+      .then((descricao: string) => {
+        this.ondeFica = descricao;
+      });
   }
 }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -39,4 +39,13 @@ export class OfertasService {
         return resposta[0].descricao;
       });
   }
+
+  public getOndeFicaOfertaPorId(id: number): Promise<string> {
+    return this.http
+      .get(`${URL_API}/onde-fica?id=${id}`)
+      .toPromise()
+      .then((resposta: any) => {
+        return resposta[0].descricao;
+      });
+  }
 }
