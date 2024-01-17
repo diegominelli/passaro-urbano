49893906dc555d4a656bce0064b6b177546dc847
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-17 17:38:25 -0300

    trabalhando com o observables

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { Observable, Observer, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-oferta',
@@ -9,7 +10,10 @@ import { Oferta } from '../shared/oferta.model';
   styleUrls: ['./oferta.component.scss'],
   providers: [OfertasService],
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
+  private tempoObservableSubscription: Subscription;
+  private meuObservableTesteSubscription: Subscription;
+
   public oferta: Oferta;
 
   constructor(
@@ -23,5 +27,36 @@ export class OfertaComponent implements OnInit {
       .then((oferta: Oferta) => {
         this.oferta = oferta;
       });
+
+    // this.route.params.subscribe(
+    //   (parametro: any) => {
+    //     console.log(parametro);
+    //   },
+    //   (erro: any) => console.log(erro),
+    //   () => console.log('Finalizou a subscrição')
+    // );
+
+    let tempo = interval(2000);
+
+    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => {
+      console.log(intervalo);
+    });
+
+    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
+      observer.next(100);
+      observer.next(1000);
+      observer.complete();
+    });
+
+    this.meuObservableTesteSubscription = meuObservableTeste.subscribe(
+      (resultado: number) => console.log(resultado + 10),
+      (erro: string) => console.log(erro),
+      () => console.log('stream de eventos foi finalizda')
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.meuObservableTesteSubscription.unsubscribe();
+    this.tempoObservableSubscription.unsubscribe();
   }
 }
