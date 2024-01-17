55277fb2b7d096ff654a22744733d9a7dce4b78d
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-17 17:52:05 -0300

    incluindo o campo de pesquisa

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
-import { Observable, Observer, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-oferta',
@@ -11,9 +10,6 @@ import { Observable, Observer, Subscription, interval } from 'rxjs';
   providers: [OfertasService],
 })
 export class OfertaComponent implements OnInit, OnDestroy {
-  private tempoObservableSubscription: Subscription;
-  private meuObservableTesteSubscription: Subscription;
-
   public oferta: Oferta;
 
   constructor(
@@ -27,36 +23,7 @@ export class OfertaComponent implements OnInit, OnDestroy {
       .then((oferta: Oferta) => {
         this.oferta = oferta;
       });
-
-    // this.route.params.subscribe(
-    //   (parametro: any) => {
-    //     console.log(parametro);
-    //   },
-    //   (erro: any) => console.log(erro),
-    //   () => console.log('Finalizou a subscrição')
-    // );
-
-    let tempo = interval(2000);
-
-    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => {
-      console.log(intervalo);
-    });
-
-    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
-      observer.next(100);
-      observer.next(1000);
-      observer.complete();
-    });
-
-    this.meuObservableTesteSubscription = meuObservableTeste.subscribe(
-      (resultado: number) => console.log(resultado + 10),
-      (erro: string) => console.log(erro),
-      () => console.log('stream de eventos foi finalizda')
-    );
   }
 
-  ngOnDestroy(): void {
-    this.meuObservableTesteSubscription.unsubscribe();
-    this.tempoObservableSubscription.unsubscribe();
-  }
+  ngOnDestroy(): void {}
 }
