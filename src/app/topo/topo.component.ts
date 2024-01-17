b662d5e2ee933ed3e8c6ecb690261c7113a5fa4f
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-17 18:04:26 -0300

    capturando a entrada do usuario via variavel de referencia

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
-  styleUrls: ['./topo.component.scss']
+  styleUrls: ['./topo.component.scss'],
 })
-export class TopoComponent {
+export class TopoComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  pesquisa(termoDaPesquisa: string): void {
+    console.log(termoDaPesquisa);
+  }
 }
