536d04a36a38355e17a58d521ddc6e09a0da77aa
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-25 15:17:03 -0300

    atualizando oferta no processo de mudanca de rota

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
 
@@ -18,11 +18,13 @@ export class OfertaComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.ofertasService
-      .getOfertaPorId(this.route.snapshot.params['id'])
-      .then((oferta: Oferta) => {
-        this.oferta = oferta;
-      });
+    this.route.params.subscribe((parametros: Params) => {
+      this.ofertasService
+        .getOfertaPorId(parametros['id'])
+        .then((oferta: Oferta) => {
+          this.oferta = oferta;
+        });
+    });
   }
 
   ngOnDestroy(): void {}
