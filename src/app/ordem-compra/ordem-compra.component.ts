3a9f5f05ae4ab1fb13f1fd3f08b2c6f3dec9a525
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-25 16:44:16 -0300

    usando o two way binding dos dados do formulario

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
-  styleUrls: ['./ordem-compra.component.scss']
+  styleUrls: ['./ordem-compra.component.scss'],
 })
-export class OrdemCompraComponent {
+export class OrdemCompraComponent implements OnInit {
+  public complemento: string = '';
+  public endereco: string = '';
+  public formaPagamento: string = '';
+  public numero: string = '';
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public atualizaEndereco(endereco: string): void {
+    console.log(endereco);
+
+    this.endereco = endereco;
+  }
+
+  public atualizaNumero(numero: string): void {
+    console.log(numero);
+    this.numero = numero;
+  }
+
+  public atualizaComplemento(complemento: string): void {
+    console.log(complemento);
+    this.complemento = complemento;
+  }
+
+  public atualizaFormaPagamento(formaPagamento: string): void {
+    console.log(formaPagamento);
+    this.formaPagamento = formaPagamento;
+  }
 }
