10e25abb8a8686f0f08da1311add0a9cee42aa4c
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-02-08 17:37:44 -0300

    diminuindo a quantidade dos itens e tratando a quantidade zero

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -59,6 +59,10 @@ export class CarrinhoService {
 
     if (itemCarrinhoEncontrado) {
       itemCarrinhoEncontrado.quantidade -= 1;
+
+      if (itemCarrinhoEncontrado.quantidade === 0) {
+        this.itens.splice(this.itens.indexOf(itemCarrinhoEncontrado), 1);
+      }
     }
   }
 }
